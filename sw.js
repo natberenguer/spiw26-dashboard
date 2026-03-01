793946e2e9a3b44f2d0896356d44fe3fe88b0995
natberenguer <nataliacamerob@gmail.com>
2026-03-01 17:16:39 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); });
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request, {cache: 'no-store'}).catch(() => caches.match(e.request)));
+});
